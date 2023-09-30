ba58e1a8e084dba97093efe80b7dc81316b2992d
seoyamin <seoyamin@ewhain.net>
2023-09-30 17:35:59 +0900

    [init] Init the Project

---
+++ src/main/java/com/letsintern/letsintern/LetsinternApplication.java
@@ -0,0 +1,13 @@
+package com.letsintern.letsintern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsinternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsinternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letsintern/letsintern/LetsinternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letsintern.letsintern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsinternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
