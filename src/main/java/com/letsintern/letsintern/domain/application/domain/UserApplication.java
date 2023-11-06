aa02663d029c8435e91818e6f7e4a76ed4ae9c29
seoyamin <hyeminseo310@gmail.com>
2023-11-06 11:01:13 +0900

    [feat] 비회원 신청 API #4

---
+++ src/main/java/com/letsintern/letsintern/domain/application/domain/UserApplication.java
@@ -0,0 +1,39 @@
+package com.letsintern.letsintern.domain.application.domain;
+
+import com.letsintern.letsintern.domain.application.dto.request.ApplicationCreateDTO;
+import com.letsintern.letsintern.domain.program.domain.Program;
+import com.letsintern.letsintern.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@DiscriminatorValue("user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserApplication extends Application {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = true)
+    private User user;
+
+    @Builder
+    private UserApplication(Program program, User user, Integer grade,
+                            String wishCompany, String wishJob, String applyMotive) {
+        this.program = program;
+        this.user = user;
+        this.grade = grade;
+        this.wishCompany = wishCompany;
+        this.wishJob = wishJob;
+        this.applyMotive = applyMotive;
+    }
+
+    public static UserApplication of(Program program, User user, ApplicationCreateDTO applicationCreateDTO) {
+        return UserApplication.builder()
+                .program(program)
+                .user(user)
+                .grade(applicationCreateDTO.getGrade())
+                .wishCompany(applicationCreateDTO.getWishCompany())
+                .wishJob(applicationCreateDTO.getWishJob())
+                .applyMotive(applicationCreateDTO.getApplyMotive())
+                .build();
+    }
+}
