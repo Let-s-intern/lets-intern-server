e532f2662c3515201f642d7a50251014f02092d1
seoyamin <hyeminseo310@gmail.com>
2023-11-06 14:48:19 +0900

    [fix] Review Swagger 수정 #18

--- src/main/java/com/letsintern/letsintern/domain/review/ReviewController.java
+++ src/main/java/com/letsintern/letsintern/domain/review/ReviewController.java
@@ -21,19 +21,19 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping("/create/{applicationId}")
-    @Operation(tags = "리뷰 생성")
+    @Operation(summary = "리뷰 생성")
     public ReviewIdResponseDTO createReview(@PathVariable Long applicationId, @RequestBody ReviewCreateDTO reviewCreateDTO) {
         return reviewService.createReview(applicationId, reviewCreateDTO);
     }
 
     @GetMapping("/admin/list/{programId}")
-    @Operation(tags = "어드민 프로그램 별 리뷰 목록")
+    @Operation(summary = "어드민 프로그램 별 리뷰 목록")
     public ReviewListResponseDTO getReviewListOfProgram(@PathVariable Long programId, @PageableDefault(size = 15) Pageable pageable) {
         return reviewService.getReviewListOfProgram(programId, pageable);
     }
 
     @GetMapping("/admin/update/{reviewId}/{status}")
-    @Operation(tags = "어드민 리뷰 상태 변경")
+    @Operation(summary = "어드민 리뷰 상태 변경")
     public ReviewIdResponseDTO updateReviewStatus(@PathVariable Long reviewId, @PathVariable ReviewStatus status) {
         return reviewService.updateReviewStatus(reviewId, status);
     }
