d844b955a19c51df9c367430a1b5485aa2f2bcbf
seoyamin <seoyamin@ewhain.net>
2023-11-17 00:16:54 +0900

    [feat] Program 상세 보기 #3

--- src/main/java/com/letsintern/letsintern/domain/program/repository/ProgramRepositoryCustom.java
+++ src/main/java/com/letsintern/letsintern/domain/program/repository/ProgramRepositoryCustom.java
@@ -1,10 +1,12 @@
 package com.letsintern.letsintern.domain.program.repository;
 
 import com.letsintern.letsintern.domain.program.domain.ProgramType;
+import com.letsintern.letsintern.domain.program.vo.ProgramDetailVo;
 import com.letsintern.letsintern.domain.program.vo.ProgramThumbnailVo;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProgramRepositoryCustom {
 
@@ -13,4 +15,6 @@ public interface ProgramRepositoryCustom {
     List<ProgramThumbnailVo> findProgramThumbnails(Pageable pageable);
 
     List<ProgramThumbnailVo> findProgramThumbnailsByType(String type, Pageable pageable);
+
+    Optional<ProgramDetailVo> findProgramDetailVo(Long programId);
 }
--- src/main/java/com/letsintern/letsintern/domain/program/service/ProgramService.java
+++ src/main/java/com/letsintern/letsintern/domain/program/service/ProgramService.java
@@ -1,11 +1,15 @@
 package com.letsintern.letsintern.domain.program.service;
 
+import com.letsintern.letsintern.domain.program.domain.Program;
 import com.letsintern.letsintern.domain.program.dto.request.ProgramCreateRequestDTO;
 import com.letsintern.letsintern.domain.program.dto.request.ProgramUpdateRequestDTO;
 import com.letsintern.letsintern.domain.program.dto.response.ProgramIdResponseDTO;
 import com.letsintern.letsintern.domain.program.dto.response.ProgramListDTO;
+import com.letsintern.letsintern.domain.program.exception.ProgramNotFound;
 import com.letsintern.letsintern.domain.program.helper.ProgramHelper;
 import com.letsintern.letsintern.domain.program.mapper.ProgramMapper;
+import com.letsintern.letsintern.domain.program.repository.ProgramRepository;
+import com.letsintern.letsintern.domain.program.vo.ProgramDetailVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -20,6 +24,8 @@ public class ProgramService {
 
     private final ProgramHelper programHelper;
     private final ProgramMapper programMapper;
+
+    private final ProgramRepository programRepository;
 
 
     @Transactional
@@ -40,5 +46,17 @@ public class ProgramService {
     @Transactional
     public ProgramListDTO getProgramTypeList(String type, Pageable pageable) {
         return programHelper.getProgramTypeList(type, pageable);
+    }
+
+    public ProgramDetailVo getProgramDetailVo(Long programId) {
+        return programHelper.getProgramDetailVo(programId);
+    }
+
+    public Program getProgram(Long programId) {
+        Program program = programRepository.findById(programId)
+                .orElseThrow(() -> {
+                    throw ProgramNotFound.EXCEPTION;
+                });
+        return program;
     }
 }
