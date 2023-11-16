bcb6544b1ab57a5efd49908659f0aad73d0e9d98
seoyamin <seoyamin@ewhain.net>
2023-11-17 00:59:46 +0900

    [feat] Faq Entity 생성 #23

---
+++ src/main/java/com/letsintern/letsintern/domain/faq/domain/Faq.java
@@ -0,0 +1,38 @@
+package com.letsintern.letsintern.domain.faq.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.letsintern.letsintern.domain.program.domain.Program;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Faq {
+
+    @Id
+    @Column(name = "faq_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String question;
+
+    @NotNull
+    private String answer;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "program_id", nullable = false)
+    @JsonIgnore
+    Program program;
+
+    @Builder
+    private Faq(Program program, String question, String answer) {
+        this.program = program;
+        this.question = question;
+        this.answer = answer;
+    }
+
+}
--- src/main/java/com/letsintern/letsintern/domain/program/vo/ProgramDetailVo.java
+++ src/main/java/com/letsintern/letsintern/domain/program/vo/ProgramDetailVo.java
@@ -1,7 +1,10 @@
 package com.letsintern.letsintern.domain.program.vo;
 
+import com.letsintern.letsintern.domain.faq.domain.Faq;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class ProgramDetailVo {
@@ -13,13 +16,16 @@ public class ProgramDetailVo {
     private String title;
     private String contents;
 
+    private List<Faq> faqList;
+
     @Builder
     public ProgramDetailVo(String userName, String userEmail, String userPhoneNum,
-                           String title, String contents) {
+                           String title, String contents, List<Faq> faqList) {
         this.userName = userName;
         this.userEmail = userEmail;
         this.userPhoneNum = userPhoneNum;
         this.title = title;
         this.contents = contents;
+        this.faqList = faqList;
     }
 }
