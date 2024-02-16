84191d131fe66c653bc597b5e9bcf83b4a2dc372
seoyamin <seoyamin@ewhain.net>
2024-02-16 13:02:37 +0900

    [fix] Attendance Status WRONG Update시 조건 추가 #64

--- src/main/java/com/letsintern/letsintern/domain/attendance/helper/AttendanceHelper.java
+++ src/main/java/com/letsintern/letsintern/domain/attendance/helper/AttendanceHelper.java
@@ -40,16 +40,18 @@ public class AttendanceHelper {
         Application application = applicationRepository.findByProgramIdAndUserId(mission.getProgram().getId(), user.getId());
         if(application == null) throw ApplicationNotFound.EXCEPTION;
 
+        boolean isLateAttendance = mission.getEndDate().isBefore(LocalDateTime.now());
+
         // 이미 출석한 경우 처리
         final Attendance attendance = attendanceRepository.findByMissionIdAndUserId(missionId, user.getId());
         if(attendance != null) throw AttendanceAlreadyExists.EXCEPTION;
 
         // Mission.attendanceCount++, Mission.lateAttendance++
         mission.setAttendanceCount(mission.getAttendanceCount() + 1);
-        if(mission.getEndDate().isBefore(LocalDateTime.now())) mission.setLateAttendanceCount(mission.getLateAttendanceCount() + 1);
+        if(isLateAttendance) mission.setLateAttendanceCount(mission.getLateAttendanceCount() + 1);
 
         // 보증금 미션인 경우, Application.refund 적립
-        if(mission.getType().equals(MissionType.REFUND)) {
+        if(mission.getType().equals(MissionType.REFUND) && !isLateAttendance) {
             application.setRefund(application.getRefund() + mission.getRefund());
         }
 
@@ -66,11 +68,28 @@ public class AttendanceHelper {
 
     public Long updateAttendanceAdmin(Long attendanceId, AttendanceAdminUpdateDTO attendanceAdminUpdateDTO) {
         Attendance attendance = attendanceRepository.findById(attendanceId).orElseThrow(() -> AttendanceNotFound.EXCEPTION);
+        Application application = applicationRepository.findByProgramIdAndUserId(attendance.getMission().getProgram().getId(), attendance.getUser().getId());
+        if(application == null) throw ApplicationNotFound.EXCEPTION;
+
         if(attendanceAdminUpdateDTO.getLink() != null) attendance.setLink(attendanceAdminUpdateDTO.getLink());
         if(attendanceAdminUpdateDTO.getStatus() != null) {
-            // 제출된 출석을 반려하는 경우
-            if(!attendance.getStatus().equals(AttendanceStatus.WRONG) && attendanceAdminUpdateDTO.getStatus().equals(AttendanceStatus.WRONG)) {
-                attendance.setComments(null);
+            if(attendance.getMission().getIsRefunded() == true) {
+
+            }
+            if(attendance.getMission().getType().equals(MissionType.REFUND)) {
+                // 수정된 출석을 반려하는 경우 (수정 -> attendance.status = UPDATED, mission.attendanceCount++)
+                if(attendance.getStatus().equals(AttendanceStatus.UPDATED) && attendanceAdminUpdateDTO.getStatus().equals(AttendanceStatus.WRONG)) {
+                    application.setRefund(application.getRefund() - attendance.getMission().getRefund());           // 환급 금액에서 제외하기
+                    attendance.getMission().setAttendanceCount(attendance.getMission().getAttendanceCount() - 1);   // 출석 개수에서 제외하기
+                    attendance.setComments(null);                                                                   // 출석 코멘트 삭제하기
+                }
+
+                // 그 외 출석을 반려하는 경우
+                if(!attendance.getStatus().equals(AttendanceStatus.WRONG) && attendanceAdminUpdateDTO.getStatus().equals(AttendanceStatus.WRONG)) {
+                    application.setRefund(application.getRefund() - attendance.getMission().getRefund());           // 환급 금액에서 제외하기
+                    attendance.getMission().setAttendanceCount(attendance.getMission().getAttendanceCount() - 1);   // 출석 개수에서 제외하기
+                }
+
             }
             attendance.setStatus(attendanceAdminUpdateDTO.getStatus());
         }
