d178bce1c060f58a63c74287249a75843041e956
RyuKwanKon <rhks33rhs@gmail.com>
2024-03-27 17:33:49 +0900

    [Feat] 기본 쿠폰 정보 enum 적용 vo 구현

---
+++ src/main/java/com/letsintern/letsintern/domain/coupon/vo/BaseCouponEnumVo.java
@@ -0,0 +1,36 @@
+package com.letsintern.letsintern.domain.coupon.vo;
+
+import com.letsintern.letsintern.domain.coupon.domain.CouponProgramType;
+import com.letsintern.letsintern.domain.coupon.domain.CouponType;
+import com.letsintern.letsintern.domain.coupon.dto.request.BaseCouponRequestDto;
+import lombok.AccessLevel;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record BaseCouponEnumVo(
+        CouponType couponType,
+        CouponProgramType couponProgramType,
+        String name,
+        String code,
+        Integer discount,
+        Integer time,
+        LocalDateTime startDate,
+        LocalDateTime endDate
+) {
+    public static BaseCouponEnumVo of(BaseCouponRequestDto baseCouponRequestDto,
+                                      CouponType couponType,
+                                      CouponProgramType couponProgramType) {
+        return BaseCouponEnumVo.builder()
+                .couponType(couponType)
+                .couponProgramType(couponProgramType)
+                .name(baseCouponRequestDto.name())
+                .code(baseCouponRequestDto.code())
+                .discount(baseCouponRequestDto.discount())
+                .time(baseCouponRequestDto.time())
+                .startDate(baseCouponRequestDto.startDate())
+                .endDate(baseCouponRequestDto.endDate())
+                .build();
+    }
+}
