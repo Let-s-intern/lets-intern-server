a1c90b04b4bc401e65bbd5b4be2f741fc3b8872c
RyuKwanKon <rhks33rhs@gmail.com>
2024-03-28 12:22:44 +0900

    [Fix] Coupon history custom repository 구현

--- src/main/java/com/letsintern/letsintern/domain/coupon/repository/CouponUserRepositoryCustom.java
+++ src/main/java/com/letsintern/letsintern/domain/coupon/repository/CouponUserRepositoryCustom.java
@@ -4,6 +4,6 @@ import com.letsintern.letsintern.domain.coupon.vo.CouponUserHistoryVo;
 
 import java.util.Optional;
 
-public interface CouponRepositoryCustom {
+public interface CouponUserRepositoryCustom {
     Optional<CouponUserHistoryVo> findCouponUserHistoryByCodeAndUserId(String code, Long userId);
 }
