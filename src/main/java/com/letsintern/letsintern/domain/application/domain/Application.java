48b0d4b61eb6ed72c18f901a742ed194d8bb9606
RyuKwanKon <rhks33rhs@gmail.com>
2024-03-28 17:55:30 +0900

    [Feat] application entity total요금 반정규화

--- src/main/java/com/letsintern/letsintern/domain/application/domain/Application.java
+++ src/main/java/com/letsintern/letsintern/domain/application/domain/Application.java
@@ -118,12 +118,12 @@ public class Application {
 
     @Nullable
     private String couponCode;
-
+    private Integer totalFee;
 
     @Builder
     private Application(Program program, User user, Integer grade, String wishCompany, ApplicationWishJob wishJob,
                              String applyMotive, String preQuestions, InflowPath inflowPath,
-                             String name, String phoneNum, String email, AccountType accountType, String accountNumber, ApplicationWay way, String couponCode) {
+                             String name, String phoneNum, String email, AccountType accountType, String accountNumber, ApplicationWay way, String couponCode, Integer totalFee) {
         this.program = program;
         this.user = user;
         this.grade = grade;
@@ -133,6 +133,7 @@ public class Application {
         this.preQuestions = preQuestions;
         this.inflowPath = inflowPath;
         this.couponCode = couponCode;
+        this.totalFee = totalFee;
         if(way != null) this.way = way;
 
         /* 비회원 */
@@ -158,7 +159,7 @@ public class Application {
         }
     }
 
-    public static Application of(Program program, User user, ApplicationCreateDTO applicationCreateDTO) {
+    public static Application of(Program program, User user, ApplicationCreateDTO applicationCreateDTO, Integer totalFee) {
         return Application.builder()
                 .program(program)
                 .user(user)
@@ -175,6 +176,7 @@ public class Application {
                 .accountNumber(applicationCreateDTO.getAccountNumber())
                 .way(applicationCreateDTO.getWay())
                 .couponCode(applicationCreateDTO.getCode())
+                .totalFee(totalFee)
                 .build();
     }
 }
