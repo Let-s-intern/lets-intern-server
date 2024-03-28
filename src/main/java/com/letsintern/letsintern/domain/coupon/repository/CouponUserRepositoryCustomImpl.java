1c35baf1d1f7f07349fbcc7891b186e4f84d744a
RyuKwanKon <rhks33rhs@gmail.com>
2024-03-28 12:23:12 +0900

    [Fix] Coupon history custom repository 구현체로 변경

--- src/main/java/com/letsintern/letsintern/domain/coupon/repository/CouponUserRepositoryCustomImpl.java
+++ src/main/java/com/letsintern/letsintern/domain/coupon/repository/CouponUserRepositoryCustomImpl.java
@@ -1,9 +1,6 @@
 package com.letsintern.letsintern.domain.coupon.repository;
 
-import com.letsintern.letsintern.domain.coupon.domain.QCoupon;
-import com.letsintern.letsintern.domain.coupon.domain.QCouponUser;
 import com.letsintern.letsintern.domain.coupon.vo.CouponUserHistoryVo;
-import com.letsintern.letsintern.domain.user.domain.QUser;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -11,28 +8,20 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Optional;
 
-import static com.letsintern.letsintern.domain.coupon.domain.QCoupon.coupon;
-import static com.letsintern.letsintern.domain.user.domain.QUser.user;
 import static com.letsintern.letsintern.domain.coupon.domain.QCouponUser.couponUser;
 
 @RequiredArgsConstructor
-public class CouponRepositoryCustomImpl implements CouponRepositoryCustom{
+public class CouponUserRepositoryCustomImpl implements CouponUserRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     @Override
     public Optional<CouponUserHistoryVo> findCouponUserHistoryByCodeAndUserId(String code, Long userId) {
         return Optional.ofNullable(queryFactory
                 .select(Projections.constructor(CouponUserHistoryVo.class,
-                        couponUser.id,
-                        user.id,
-                        coupon.discount,
-                        couponUser.remainTime,
-                        coupon.startDate,
-                        coupon.endDate
+                        couponUser.coupon,
+                        couponUser.user
                 ))
                 .from(couponUser)
-                .leftJoin(couponUser.coupon, coupon)
-                .leftJoin(couponUser.user, user)
                 .where(
                         eqUserId(userId),
                         eqCouponCode(code)
@@ -41,10 +30,10 @@ public class CouponRepositoryCustomImpl implements CouponRepositoryCustom{
     }
 
     private BooleanExpression eqUserId(Long userId) {
-        return userId != null ? user.id.eq(userId) : null;
+        return userId != null ? couponUser.user.id.eq(userId) : null;
     }
 
     private BooleanExpression eqCouponCode(String code) {
-        return code != null ? coupon.code.eq(code) : null;
+        return code != null ? couponUser.coupon.code.eq(code) : null;
     }
 }
