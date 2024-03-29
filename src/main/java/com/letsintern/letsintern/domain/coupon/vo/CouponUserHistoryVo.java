2d84b985b5bc4f28dce7ac64d707361a9e9a7148
RyuKwanKon <rhks33rhs@gmail.com>
2024-03-29 10:51:24 +0900

    [Fix] 데이터베이스 접근을 위한 builder access 범위 제한 변경

--- src/main/java/com/letsintern/letsintern/domain/coupon/vo/CouponUserHistoryVo.java
+++ src/main/java/com/letsintern/letsintern/domain/coupon/vo/CouponUserHistoryVo.java
@@ -2,10 +2,9 @@ package com.letsintern.letsintern.domain.coupon.vo;
 
 import com.letsintern.letsintern.domain.coupon.domain.Coupon;
 import com.letsintern.letsintern.domain.user.domain.User;
-import lombok.AccessLevel;
 import lombok.Builder;
 
-@Builder(access = AccessLevel.PRIVATE)
+@Builder
 public record CouponUserHistoryVo(
         Coupon coupon,
         User user,
