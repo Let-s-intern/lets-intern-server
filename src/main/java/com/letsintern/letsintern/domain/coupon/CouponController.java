4b965e88c57d766f3e25b985cff2cb3f439981de
RyuKwanKon <rhks33rhs@gmail.com>
2024-03-29 10:46:42 +0900

    [Feat] admin api와 user api 메서드 명 적용

--- src/main/java/com/letsintern/letsintern/domain/coupon/CouponController.java
+++ src/main/java/com/letsintern/letsintern/domain/coupon/CouponController.java
@@ -1,11 +1,14 @@
 package com.letsintern.letsintern.domain.coupon;
 
 import com.letsintern.letsintern.domain.coupon.dto.request.BaseCouponRequestDto;
+import com.letsintern.letsintern.domain.coupon.dto.response.CouponAllResponseDto;
 import com.letsintern.letsintern.domain.coupon.dto.response.CouponApplyResponseDto;
 import com.letsintern.letsintern.domain.coupon.service.CouponService;
 import com.letsintern.letsintern.global.config.user.PrincipalDetails;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +20,12 @@ import org.springframework.web.bind.annotation.*;
 public class CouponController {
     private final CouponService couponService;
 
+    @GetMapping
+    public ResponseEntity<CouponAllResponseDto> getCouponsForAdmin(@PageableDefault(size = 10) final Pageable pageable) {
+        final CouponAllResponseDto responseDto = couponService.getCoupons(pageable);
+        return ResponseEntity.ok(responseDto);
+    }
+
     @GetMapping("/{code}")
     public ResponseEntity<CouponApplyResponseDto> getAvailableCoupon(@AuthenticationPrincipal final PrincipalDetails principalDetails,
                                                                      @PathVariable("code") final String code) {
@@ -25,13 +34,18 @@ public class CouponController {
     }
 
     @PostMapping
-    public void createNewCoupon(@RequestBody final BaseCouponRequestDto baseCouponRequestDto) {
+    public void createNewCouponForAdmin(@RequestBody final BaseCouponRequestDto baseCouponRequestDto) {
         couponService.createNewCoupon(baseCouponRequestDto);
     }
 
     @PatchMapping("/{id}")
-    public void updateCouponInfo(@PathVariable("id") final Long couponId,
+    public void updateCouponInfoForAdmin(@PathVariable("id") final Long couponId,
                                  @RequestBody final BaseCouponRequestDto baseCouponRequestDto) {
         couponService.updateCouponInfo(couponId, baseCouponRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCouponForAdmin(@PathVariable("id") final Long couponId) {
+        couponService.deleteCoupon(couponId);
     }
 }
