3c0f3b0901276b4c19f62b3aaabab6fcdab47381
RyuKwanKon <rhks33rhs@gmail.com>
2024-03-29 10:48:47 +0900

    [Feat] vo to dto 메서드 구현

--- src/main/java/com/letsintern/letsintern/domain/coupon/mapper/CouponMapper.java
+++ src/main/java/com/letsintern/letsintern/domain/coupon/mapper/CouponMapper.java
@@ -4,8 +4,12 @@ import com.letsintern.letsintern.domain.coupon.domain.Coupon;
 import com.letsintern.letsintern.domain.coupon.domain.CouponProgramType;
 import com.letsintern.letsintern.domain.coupon.domain.CouponType;
 import com.letsintern.letsintern.domain.coupon.dto.request.BaseCouponRequestDto;
+import com.letsintern.letsintern.domain.coupon.dto.response.CouponAllResponseDto;
 import com.letsintern.letsintern.domain.coupon.vo.BaseCouponEnumVo;
+import com.letsintern.letsintern.domain.coupon.vo.CouponAdminVo;
+import com.letsintern.letsintern.global.common.dto.PageInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import static com.letsintern.letsintern.global.utils.EnumValueUtils.toEntityCode;
@@ -21,5 +25,10 @@ public class CouponMapper {
         CouponType couponType = toEntityCode(CouponType.class, baseCouponRequestDto.couponType());
         CouponProgramType couponProgramType = toEntityCode(CouponProgramType.class, baseCouponRequestDto.programType());
         return BaseCouponEnumVo.of(baseCouponRequestDto, couponType, couponProgramType);
+    }
+
+    public CouponAllResponseDto toCouponAllResponseDto(Page<CouponAdminVo> couponList) {
+        PageInfo pageInfo = PageInfo.of(couponList);
+        return CouponAllResponseDto.of(couponList, pageInfo);
     }
 }
