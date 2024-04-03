10f56bdec6d2a2e7db4eb7d57df6ad92777d6d5f
seoyamin <seoyamin@ewhain.net>
2024-04-03 23:26:59 +0900

    [feat] Banner Entity 생성 #123

---
+++ src/main/java/com/letsintern/letsintern/domain/banner/domain/Banner.java
@@ -0,0 +1,32 @@
+package com.letsintern.letsintern.domain.banner.domain;
+
+import com.letsintern.letsintern.domain.banner.domain.converter.BannerStatusConverter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "Banner")
+@Getter
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Banner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    @Builder.Default
+    @Convert(converter = BannerStatusConverter.class)
+    private BannerStatus status = BannerStatus.VALID;
+
+    @Builder.Default
+    private Boolean isVisible = false;
+}
