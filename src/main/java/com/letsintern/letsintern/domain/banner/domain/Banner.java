b9529a6b04f385e99f9a975043a0488294328456
seoyamin <seoyamin@ewhain.net>
2024-04-04 23:07:13 +0900

    [feat] MainBanner Entity 생성 #123

--- src/main/java/com/letsintern/letsintern/domain/banner/domain/Banner.java
+++ src/main/java/com/letsintern/letsintern/domain/banner/domain/Banner.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public abstract class Banner {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
 
     private String title;
