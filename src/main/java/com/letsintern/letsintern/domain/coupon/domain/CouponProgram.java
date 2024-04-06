264a84d757a74af39526232bdf228e2fdb814cd2
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-06 18:06:07 +0900

    [Fix] CouponProgramType 중복선택으로 Table 생성

---
+++ src/main/java/com/letsintern/letsintern/domain/coupon/domain/CouponProgram.java
@@ -0,0 +1,31 @@
+package com.letsintern.letsintern.domain.coupon.domain;
+
+import com.letsintern.letsintern.domain.coupon.domain.converter.CouponProgramTypeConverter;
+import com.letsintern.letsintern.domain.coupon.vo.BaseCouponProgramEnumVo;
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class CouponProgram {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "coupon_program_id")
+    private Long id;
+    @Convert(converter = CouponProgramTypeConverter.class)
+    private CouponProgramType couponProgramType;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coupon_id")
+    private Coupon coupon;
+
+    public static CouponProgram createCouponProgram(BaseCouponProgramEnumVo baseCouponProgramEnumVo, Coupon coupon) {
+        CouponProgram couponProgram = CouponProgram.builder()
+                .couponProgramType(baseCouponProgramEnumVo.couponProgramType())
+                .build();
+        coupon.addCouponProgramType(couponProgram);
+        return couponProgram;
+    }
+}
