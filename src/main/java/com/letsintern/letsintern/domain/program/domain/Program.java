9d7fb3704952f64817155caf47d43107d7da63e3
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-10 19:57:55 +0900

    [Feat] Program decrease 메서드 추가

--- src/main/java/com/letsintern/letsintern/domain/program/domain/Program.java
+++ src/main/java/com/letsintern/letsintern/domain/program/domain/Program.java
@@ -107,6 +107,10 @@ public abstract class Program {
         this.applicationCount++;
     }
 
+    public void decreaseProgramApplicationCount() {
+        this.applicationCount--;
+    }
+
     public void updateProgramInfo(ProgramRequestDto programRequestDto,
                                   ProgramStatus programStatus,
                                   String stringFaqList) {
