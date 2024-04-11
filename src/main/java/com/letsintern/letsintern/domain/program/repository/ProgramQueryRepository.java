0e882aa42315aaa216a9e41d3df7f7d0a413f95f
seoyamin <seoyamin@ewhain.net>
2024-04-11 09:45:11 +0900

    [refactor] Program Status 변경 Batch side effect 정리 #131

--- src/main/java/com/letsintern/letsintern/domain/program/repository/ProgramQueryRepository.java
+++ src/main/java/com/letsintern/letsintern/domain/program/repository/ProgramQueryRepository.java
@@ -2,8 +2,14 @@ package com.letsintern.letsintern.domain.program.repository;
 
 import com.letsintern.letsintern.domain.program.vo.program.ProgramDetailVo;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface ProgramQueryRepository {
     Optional<ProgramDetailVo> findProgramDetailVo(Long programId);
+
+    void updateAllProgramStatusClosedByDueDate(LocalDateTime now);
+
+    List<Long> findAllProgramIdListAndUpdateStatusToDone(LocalDateTime now);
 }
--- src/main/java/com/letsintern/letsintern/domain/program/repository/ProgramRepositoryImpl.java
+++ src/main/java/com/letsintern/letsintern/domain/program/repository/ProgramRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.letsintern.letsintern.domain.program.repository;
 
+import com.letsintern.letsintern.domain.program.domain.ProgramStatus;
 import com.letsintern.letsintern.domain.program.vo.program.ProgramDetailVo;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static com.letsintern.letsintern.domain.payment.domain.QPayment.payment;
@@ -16,6 +20,7 @@ import static com.letsintern.letsintern.domain.program.domain.QProgram.program;
 @Repository
 public class ProgramRepositoryImpl implements ProgramQueryRepository {
     private final JPAQueryFactory jpaQueryFactory;
+    private final EntityManager em;
 
     @Override
     public Optional<ProgramDetailVo> findProgramDetailVo(Long programId) {
@@ -48,7 +53,59 @@ public class ProgramRepositoryImpl implements ProgramQueryRepository {
                 ).fetchOne());
     }
 
+    @Override
+    public void updateAllProgramStatusClosedByDueDate(LocalDateTime now) {
+        jpaQueryFactory
+                .update(program)
+                .set(program.status, ProgramStatus.CLOSED)
+                .where(
+                        eqProgramStatus(ProgramStatus.OPEN),
+                        isClosedProgram(now)
+                )
+                .execute();
+
+        em.flush();
+        em.clear();
+    }
+
+    @Override
+    public List<Long> findAllProgramIdListAndUpdateStatusToDone(LocalDateTime now) {
+        List<Long> programIdList = jpaQueryFactory
+                .select(program.id).from(program)
+                .where(
+                        eqProgramStatus(ProgramStatus.CLOSED),
+                        isDoneProgram(now)
+                )
+                .fetch();
+
+        jpaQueryFactory
+                .update(program)
+                .set(program.status, ProgramStatus.DONE)
+                .where(
+                        eqProgramStatus(ProgramStatus.CLOSED),
+                        isDoneProgram(now)
+                )
+                .execute();
+
+        em.flush();
+        em.clear();
+
+        return programIdList;
+    }
+
     private BooleanExpression eqProgramId(Long programId) {
         return programId != null ? program.id.eq(programId) : null;
+    }
+
+    private BooleanExpression eqProgramStatus(ProgramStatus programStatus) {
+        return program.status.eq(programStatus);
+    }
+
+    private BooleanExpression isClosedProgram(LocalDateTime now) {
+        return program.dueDate.before(now);
+    }
+
+    private BooleanExpression isDoneProgram(LocalDateTime now) {
+        return program.endDate.before(now);
     }
 }
