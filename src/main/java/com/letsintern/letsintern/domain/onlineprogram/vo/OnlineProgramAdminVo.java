774d928edfe74372e639dc63b4a6da51ba286c36
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-13 00:02:24 +0900

    [Feat] invisible & createdAt 데이터 추가 조회

--- src/main/java/com/letsintern/letsintern/domain/onlineprogram/vo/OnlineProgramAdminVo.java
+++ src/main/java/com/letsintern/letsintern/domain/onlineprogram/vo/OnlineProgramAdminVo.java
@@ -2,12 +2,16 @@ package com.letsintern.letsintern.domain.onlineprogram.vo;
 
 import lombok.Builder;
 
+import java.time.LocalDateTime;
+
 @Builder
 public record OnlineProgramAdminVo(
         Long id,
         String title,
         String description,
         String link,
-        String thumbnailUrl
+        String thumbnailUrl,
+        boolean isVisible,
+        LocalDateTime createdAt
 ) {
 }
