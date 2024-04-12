591edf92c658a879d19270e1fa0c760185dd8dd7
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-13 00:02:05 +0900

    [Feat] invisible 데이터 추가

--- src/main/java/com/letsintern/letsintern/domain/onlineprogram/dto/request/OnlineProgramUpdateDTO.java
+++ src/main/java/com/letsintern/letsintern/domain/onlineprogram/dto/request/OnlineProgramUpdateDTO.java
@@ -3,6 +3,7 @@ package com.letsintern.letsintern.domain.onlineprogram.dto.request;
 public record OnlineProgramUpdateDTO(
         String title,
         String description,
-        String link
+        String link,
+        boolean isVisible
 ) {
 }
