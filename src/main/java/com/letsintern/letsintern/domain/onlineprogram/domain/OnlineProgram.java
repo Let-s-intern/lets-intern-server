f2930d05132a5423db8c6b0c28bd61084c4606b1
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-13 00:01:56 +0900

    [Feat] invisible 데이터 추가

--- src/main/java/com/letsintern/letsintern/domain/onlineprogram/domain/OnlineProgram.java
+++ src/main/java/com/letsintern/letsintern/domain/onlineprogram/domain/OnlineProgram.java
@@ -3,8 +3,11 @@ package com.letsintern.letsintern.domain.onlineprogram.domain;
 import com.letsintern.letsintern.domain.file.vo.S3SavedFileVo;
 import com.letsintern.letsintern.domain.onlineprogram.dto.request.OnlineProgramCreateDTO;
 import com.letsintern.letsintern.domain.onlineprogram.dto.request.OnlineProgramUpdateDTO;
+import com.letsintern.letsintern.global.common.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 import static com.letsintern.letsintern.global.utils.EntityUpdateValueUtils.updateValue;
 
@@ -14,8 +17,7 @@ import static com.letsintern.letsintern.global.utils.EntityUpdateValueUtils.upda
 @Builder(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class OnlineProgram {
-
+public class OnlineProgram extends BaseTimeEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "online_program_id")
@@ -28,6 +30,8 @@ public class OnlineProgram {
     private String link;
 
     private String thumbnailUrl;
+
+    private boolean isVisible = false;
 
     public static OnlineProgram createOnlineProgram(OnlineProgramCreateDTO onlineProgramCreateDTO, String s3Url) {
         return OnlineProgram.builder()
@@ -43,6 +47,7 @@ public class OnlineProgram {
             this.title = updateValue(this.title, onlineProgramUpdateDTO.title());
             this.description = updateValue(this.description, onlineProgramUpdateDTO.description());
             this.link = updateValue(this.link, onlineProgramUpdateDTO.link());
+            this.isVisible = updateValue(this.isVisible, onlineProgramUpdateDTO.isVisible());
         }
 
         if(s3SavedFileVo != null) {
